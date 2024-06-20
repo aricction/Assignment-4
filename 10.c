6b850cd21f1c455a31382349b972d90ba1f89b62
Govind <govindpurty350@gmail.com>
2024-06-20 09:03:54 +0530

    assignment4

---
+++ 10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int i,j=5;
+
+    for(i=1;i<=10;i++)
+
+        printf("%d*%d= %d\n",j,i,j*i);
+
+}
---
+++ 5.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+    int i;
+
+    for(i=20;i>=1;i--)
+    {
+         if( i%2==1)
+      {
+        printf("%d ",i);
+       }
+
+
+}
+}
+
+
